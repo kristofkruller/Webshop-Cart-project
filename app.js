bcf633feea57b584952fbc9e51882078b0b67e3d
Kristof Kruller <kristof.kruller@gmail.com>
2021-01-29 17:47:04 +0100

    display UI items with  coding elements, forEach and innerHTML

--- app.js
+++ app.js
@@ -33,7 +33,26 @@ class Products{
 }
 // display products
 class UI{
-
+    displayProducts(products){
+        let result = '';
+        products.forEach(product => {
+            result += `
+            <aricle class="product">
+                <div class="img-container">
+                    <img src=${product.image} alt="product"
+                    class="product-img">
+                    <button class="bag-btn" data-id=${product.id}>
+                    <i class="fas fa-shopping-cart"></i>
+                    add to bag
+                    </button>
+                </div>
+                <h3>${product.title}</h3>
+                <h4>${product.price}</h4>
+            </aricle>
+            `;    
+        });
+        productsDOM.innerHTML = result;
+    }
 }
 // local storage
 class Storage{
@@ -45,6 +64,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const products = new Products();
 
     // get all products
-    products.getProducts().then(products => console.log(products));
+    products.getProducts().then(products => ui.displayProducts(products));
 
 });
