b5e579767674b3f0c246c351275b272bd47c51af
Kristof Kruller <kristof.kruller@gmail.com>
2021-01-31 21:41:40 +0100

    remove cart buttons remove from cart and also from DOM

--- app.js
+++ app.js
@@ -140,6 +140,9 @@ class UI {
         cartContent.addEventListener('click', event => {
             if(event.target.classList.contains('remove-item')){
                 let removeItem = event.target;    
+                let id = removeItem.dataset.id;
+                cartContent.removeChild(removeItem.parentElement.parentElement);
+                this.removeItem(id);
             }      
         });
     }
